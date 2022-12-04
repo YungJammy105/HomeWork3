539a0a95a829be921d63f87da5ad495953af5345
Ilya Andreev <Franklylakers@yandex.ru>
2022-12-05 01:23:51 +0700

    add Example19

---
+++ Example19/Program.cs
@@ -0,0 +1,46 @@
+﻿bool Int = int.TryParse(Console.ReadLine(), out int num);
+int[] array = new int [CountDigit(num)];
+
+if(ItIsPalindrome(FromNumberToArray(array, num)))
+{
+    Console.WriteLine("This number is a palindrome");
+}
+else
+{
+    Console.WriteLine("This number is not a palindrome");
+}
+
+bool ItIsPalindrome (int[] col)
+{
+    int position = 0;
+    while(position < col.Length)
+    {
+        if(!(col[position] == (col[(col.Length-1)-position]))) return false;
+        position++;
+    }
+    return true;
+}
+
+int[] FromNumberToArray(int[] arr, int digit)
+{
+    int index = arr.Length-1;
+    int[] array = arr;
+    while(index >= 0)
+    {
+        array[index] = digit % 10;
+        digit = digit / 10;
+        index--;
+    }
+    return array;
+}
+
+int CountDigit(int number)
+{
+    int i = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
